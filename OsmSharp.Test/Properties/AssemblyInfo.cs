1fcda829c66edee03d9cc44b4d9d0a2c8361e669
xivk <ben@osmsharp.com>
2016-02-06 11:03:25 +0000

    Moved base OSM objects to OsmSharp namespace.

--- OsmSharp.Test/Properties/AssemblyInfo.cs
+++ OsmSharp.Test/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("OsmSharp.Test")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7697dd1e-2353-4470-a784-2b65d1ef90e9")]
+[assembly: Guid("f018f0c6-769a-49b3-84ef-3ca6533b4315")]
 
 // Version information for an assembly consists of the following four values:
 //

200a29a0b6174fbb57a8b5529b0a900050b68490
Ben Abelshausen <ben.abelshausen@gmail.com>
2017-12-11 10:59:02 +0000

    Update SharedAssemblyVersion.cs

--- SharedAssemblyVersion.cs
+++ SharedAssemblyVersion.cs
@@ -22,5 +22,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyInformationalVersion("6.0.0-pre06")]
+[assembly: AssemblyInformationalVersion("6.0.0-pre07")]
 [assembly: AssemblyVersion("6.0.0")]

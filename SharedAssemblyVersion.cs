85bcd2240907e17f9faed8f20faf08abc3f26a94
Ben Abelshausen <ben@osmsharp.com>
2018-01-03 15:10:14 +0000

    Bumped version #.

--- SharedAssemblyVersion.cs
+++ SharedAssemblyVersion.cs
@@ -22,5 +22,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyInformationalVersion("6.0.0-pre08")]
+[assembly: AssemblyInformationalVersion("6.0.0-pre09")]
 [assembly: AssemblyVersion("6.0.0")]

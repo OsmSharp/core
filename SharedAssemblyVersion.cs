13cecb26be0cf7510c37122ce759caf3f9a7d9cd
xivk <ben@itinero.tech>
2018-09-11 16:49:56 +0100

    Updated version #.

--- SharedAssemblyVersion.cs
+++ SharedAssemblyVersion.cs
@@ -22,5 +22,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyInformationalVersion("6.0.2-pre01")]
+[assembly: AssemblyInformationalVersion("6.0.2-pre02")]
 [assembly: AssemblyVersion("6.0.2")]
